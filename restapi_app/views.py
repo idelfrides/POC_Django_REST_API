37dbae16f93014641082558514fb14cec3391c1b
idelfrides <idelfridesjorgepapai@gmail.com>
2019-12-04 19:51:28 -0300

    Some changes

--- restapi_app/views.py
+++ restapi_app/views.py
@@ -8,12 +8,13 @@ import json, os
    views methods begin from here 
  -------------------------------------------
 """
+
 def resthome(request):
     data = {}
-    data['wm'] = 'IDELFRIDES JORGE | War Machine Never Give Up'
+    data['wm'] = 'IDELFRIDES JORGE'
     data['desc'] = 'This is project make a proof of concept to django REST framework.'
     data['title'] = 'Home | Django REST API '
-    data['verb'] = ''
+    data['verb'] = 'POST'
     return render(request, 'resthome.html', data)
 
 
